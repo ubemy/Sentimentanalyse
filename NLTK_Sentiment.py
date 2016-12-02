fa22468958b6e19934ccbc279209cccd1b86ac34
Maik <Maik@DESKTOP-V331FP2>
2016-12-02 13:54:09 +0100

    Stopwords und neutrale Wörter herausfiltern.

--- NLTK_Sentiment.py
+++ NLTK_Sentiment.py
@@ -2,6 +2,8 @@ from nltk.classify import NaiveBayesClassifier
 from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import *
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.corpus import subjectivity
 import os
 import codecs
 
@@ -12,29 +14,44 @@ testDataCategories = []
 testDataCategories.append("+")
 testDataCategories.append("-")
 
-sentences = []
-categories = []
+
+
+
 allWords = []
 
+stop_words = set(stopwords.words('german'))
+neutral_words = []
+
+
+def removeUnwantedWords(word):
+    returnValue = True
+    if len(word) < 3: returnValue = False
+    elif word in stop_words: returnValue = False
+    elif word in neutral_words: returnValue = False
+    return returnValue
+
 # Trainingsdaten einlesen
-def readFilesInCategory():
+def readFilesInCategory(i):
+    sentences = []
+    categories = []
     testDataLength = len(testDataDirs)
-    for i in range(0, testDataLength):
-        path = testDataDirs[i]
-        category = testDataCategories[i]
-        files = os.listdir(path)
-        text_list = []
-        for file_name in files:
-            f = open(path + "\\" + file_name, "rU", encoding="latin-1")
-            tokens = word_tokenize(f.read())
-            lower_tokens = []
-            for token in tokens:
-                lower_tokens.append(token.lower())
-            tokens = lower_tokens
-            sentences.append(tokens)
-            categories.append(category)
-            f.close()
+    #for i in range(0, testDataLength):
+    path = testDataDirs[i]
+    category = testDataCategories[i]
+    files = os.listdir(path)
+    text_list = []
+    for file_name in files:
+        f = open(path + "\\" + file_name, "rU", encoding="utf-8", errors="ignore")
+        tokens = word_tokenize(f.read())
+        lower_tokens = []
+        for token in tokens:
+            lowerToken = token.lower()
+            if(removeUnwantedWords(lowerToken)):
+                lower_tokens.append(lowerToken)
+        sentences.append(lower_tokens)
+        categories.append(category)
         allWords.append(lower_tokens)
+        f.close()
     return list(zip(sentences,categories))
 
 def getFeatures(sentence):
@@ -44,10 +61,28 @@ def getFeatures(sentence):
         features["contains({})".format(word)] = (word in allWords)
     return features
 
+def readNeutralWords():
+    path = "Testdaten\\neutral"
+    files = os.listdir(path)
+    for file_name in files:
+        f = open(path + "\\" + file_name, "rU", encoding="utf-8", errors="ignore")
+        tokens = word_tokenize(f.read())
+        for token in tokens:
+            if(token not in neutral_words):
+                neutral_words.append(token.lower())
+        f.close()
+
 def main():
-    sentences = readFilesInCategory()
-    train_docs = sentences
-    test_docs = sentences
+    readNeutralWords()
+    
+    sentences_pos = readFilesInCategory(0)
+    sentences_neg = readFilesInCategory(1)
+    train_docs_pos = sentences_pos[:189]
+    train_docs_neg = sentences_neg[:247]
+    test_docs_pos = sentences_pos[189:]
+    test_docs_neg = sentences_neg[247:]
+    train_docs = train_docs_pos+train_docs_neg
+    test_docs = test_docs_pos+test_docs_neg
 
     sentim_analyzer = SentimentAnalyzer()
     sentim_analyzer.add_feat_extractor(getFeatures)
@@ -56,7 +91,7 @@ def main():
 
     trainer = nltk.NaiveBayesClassifier.train
     classifier = sentim_analyzer.train(trainer, training_set)
-    classifier.show_most_informative_features(5)
+    classifier.show_most_informative_features(10)
 
     for key,value in sorted(sentim_analyzer.evaluate(test_set, f_measure=False, recall=False).items()):
         print('{0}: {1}'.format(key, value))	
