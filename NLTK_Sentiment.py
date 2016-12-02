4bf07ff2452642ee438cd0449f82c23e8871ef31
Maik <Maik@DESKTOP-V331FP2>
2016-12-02 15:39:55 +0100

    updates

--- NLTK_Sentiment.py
+++ NLTK_Sentiment.py
@@ -25,40 +25,43 @@ neutral_words = []
 
 def removeUnwantedWords(word):
     returnValue = True
-    if len(word) < 3: returnValue = False
-    elif word in stop_words: returnValue = False
-    elif word in neutral_words: returnValue = False
+    #if len(word) < 2: returnValue = False
+    #elif word in stop_words: returnValue = False
+    #elif word in neutral_words: returnValue = False
     return returnValue
 
 # Trainingsdaten einlesen
-def readFilesInCategory(i):
+def readFilesInCategory():
     sentences = []
     categories = []
     testDataLength = len(testDataDirs)
-    #for i in range(0, testDataLength):
-    path = testDataDirs[i]
-    category = testDataCategories[i]
-    files = os.listdir(path)
-    text_list = []
-    for file_name in files:
-        f = open(path + "\\" + file_name, "rU", encoding="utf-8", errors="ignore")
-        tokens = word_tokenize(f.read())
-        lower_tokens = []
-        for token in tokens:
-            lowerToken = token.lower()
-            if(removeUnwantedWords(lowerToken)):
-                lower_tokens.append(lowerToken)
-        sentences.append(lower_tokens)
-        categories.append(category)
-        allWords.append(lower_tokens)
-        f.close()
+    for i in range(0, testDataLength):
+        path = testDataDirs[i]
+        category = testDataCategories[i]
+        files = os.listdir(path)
+        text_list = []
+        for file_name in files:
+            f = open(path + "\\" + file_name, "rU", encoding="utf-8", errors="ignore")
+            tokens = word_tokenize(f.read())
+            lower_tokens = []
+            for token in tokens:
+                lowerToken = token.lower()
+                allWords.append(lowerToken)
+                if(removeUnwantedWords(lowerToken)):
+                    lower_tokens.append(lowerToken)
+            sentences.append(lower_tokens)
+            categories.append(category)
+            f.close()
     return list(zip(sentences,categories))
 
 def getFeatures(sentence):
+    #Die 2000 am häufigsten vorkommenden Wörter
+    all_words = nltk.FreqDist(allWords)
+    word_features = list(all_words)[:500]
     sentenceWords = set(sentence)
     features = {}
-    for word in sentenceWords:
-        features["contains({})".format(word)] = (word in allWords)
+    for word in word_features:
+        features["contains({})".format(word)] = (word in sentenceWords)
     return features
 
 def readNeutralWords():
@@ -75,14 +78,11 @@ def readNeutralWords():
 def main():
     readNeutralWords()
     
-    sentences_pos = readFilesInCategory(0)
-    sentences_neg = readFilesInCategory(1)
-    train_docs_pos = sentences_pos[:189]
-    train_docs_neg = sentences_neg[:247]
-    test_docs_pos = sentences_pos[189:]
-    test_docs_neg = sentences_neg[247:]
-    train_docs = train_docs_pos+train_docs_neg
-    test_docs = test_docs_pos+test_docs_neg
+    sentences = readFilesInCategory()
+    random.shuffle(sentences)
+    
+    train_docs = sentences[:435]
+    test_docs = sentences[435:]
 
     sentim_analyzer = SentimentAnalyzer()
     sentim_analyzer.add_feat_extractor(getFeatures)
@@ -101,6 +101,7 @@ def main():
     print("Classifying: 'Zu den negativen Folgen von zu schnellem Fahren gehört unter anderem eine Verminderung des Geschwindigkeitsgefühls.'", sentim_analyzer.classify('Die negativen Folgen von zu schnellem Fahren ist unter anderem eine Verminderung des Geschwindigkeitsgefühls.'))
     print("Classifying: 'Das Beste vom Besten reicht für eine erstklassige Bewertung in den schönsten Gegenden der Welt'", sentim_analyzer.classify('Das Beste vom Besten reicht für eine erstklassige Bewertung in den schönsten Gegenden der Welt'))
     print("Classifying: 'Die Vorlesungen ist um 8:00.'", sentim_analyzer.classify('Die Vorlesungen ist um 8:00.'))
+    print(sentim_analyzer.classify('forschung'))
 
 if __name__ == "__main__":
     main()
